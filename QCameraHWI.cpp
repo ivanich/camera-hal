sonyxperiadev/camera

HAL: set RecodringHint to TRUE when in camcorder mode CR-fixed: 335742
2 output using recordingHint to distinguish camera and camcorder mode in
mm-camera for preview and video frames divert. HAL need to add logic to
internally set this parameter if Apps don't set RecordingHint to TRUE
when in camcorder mode.

Change-Id: I5ceb99dee84697b7aea9b89e0d806e5f2fd4055b

QCameraHWI.cpp
@@ -1113,6 +1113,15 @@ void QCameraHardwareInterface::stopPreview()
     LOGI("%s: stopPreview: E", __func__);
     Mutex::Autolock lock(mLock);
     mFaceDetectOn = false;
+
+    // reset recording hint to the value passed from Apps
+    const char * str = mParameters.get(CameraParameters::KEY_RECORDING_HINT);
+    if((str != NULL) && !strcmp(str, "true")){
+        mRecordingHint = TRUE;
+    } else {
+        mRecordingHint = FALSE;
+    }
+
     switch(mPreviewState) {
       case QCAMERA_HAL_PREVIEW_START:
           //mPreviewWindow = NULL;
@@ -1203,6 +1212,20 @@ status_t QCameraHardwareInterface::startRecording()
         ret = UNKNOWN_ERROR;
         break;
     case QCAMERA_HAL_PREVIEW_STARTED:
+        if (mRecordingHint == FALSE) {
+            LOGE("%s: start recording when hint is false, stop preview first", __func__);
+            stopPreviewInternal();
+            mPreviewState = QCAMERA_HAL_PREVIEW_STOPPED;
+
+            // Set recording hint to TRUE
+            mRecordingHint = TRUE;
+            setRecordingHintValue(mRecordingHint);
+
+            // start preview again
+            mPreviewState = QCAMERA_HAL_PREVIEW_START;
+            if (startPreview2() == NO_ERROR)
+                mPreviewState = QCAMERA_HAL_PREVIEW_STARTED;
+        }
         ret =  mStreamRecord->start();
         if (MM_CAMERA_OK != ret){
             LOGE("%s: error - mStreamRecord->start!", __func__);

ivanich/camera-hal

Conflicts:
	QCameraHWI.cpp
	QCameraHWI_Parm.cpp

QCameraHWI.cpp
@@ -1145,6 +1145,15 @@ void QCameraHardwareInterface::stopPreview()
     ALOGI("%s: stopPreview: E", __func__);
     Mutex::Autolock lock(mLock);
     mFaceDetectOn = false;
+
+    // reset recording hint to the value passed from Apps
+    const char * str = mParameters.get(CameraParameters::KEY_RECORDING_HINT);
+    if((str != NULL) && !strcmp(str, "true")){
+        mRecordingHint = TRUE;
+    } else {
+        mRecordingHint = FALSE;
+    }
+
     switch(mPreviewState) {
       case QCAMERA_HAL_PREVIEW_START:
           //mPreviewWindow = NULL;
@@ -1235,8 +1244,19 @@ status_t QCameraHardwareInterface::startRecording()
         ret = UNKNOWN_ERROR;
         break;
     case QCAMERA_HAL_PREVIEW_STARTED:
-        if(!mFullLiveshotEnabled) {
-            prepareVideoPicture(true);
+        if (mRecordingHint == FALSE) {
+            ALOGE("%s: start recording when hint is false, stop preview first", __func__);
+            stopPreviewInternal();
+            mPreviewState = QCAMERA_HAL_PREVIEW_STOPPED;
+
+            // Set recording hint to TRUE
+            mRecordingHint = TRUE;
+            setRecordingHintValue(mRecordingHint);
+
+            // start preview again
+            mPreviewState = QCAMERA_HAL_PREVIEW_START;
+            if (startPreview2() == NO_ERROR)
+                mPreviewState = QCAMERA_HAL_PREVIEW_STARTED;
         }
         ret =  mStreamRecord->start();
         if (MM_CAMERA_OK != ret){